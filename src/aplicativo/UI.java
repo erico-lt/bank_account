4f5eb0218277036aae931dbf22832281dcfec26f
Érico Santana <75767727+erico-lt@users.noreply.github.com>
2022-12-06 06:12:21 -0300

    Sistema de cores
    Adicionando cores para o terminal

--- src/aplicativo/UI.java
+++ src/aplicativo/UI.java
@@ -12,8 +12,13 @@ import entites.accounts.SavingAccount;
 import entites.bank.Bank;
 import enums.Type;
 
-public class UI {    
-    
+public class UI {   
+ 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+   
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
